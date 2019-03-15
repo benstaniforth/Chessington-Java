6246c64bc967cf86f3ea2c998fa3d472606dae27
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-03-15 15:41:05 +0000

    Fixed and completed Rook moves

--- src/main/java/training/chessington/model/pieces/Rook.java
+++ src/main/java/training/chessington/model/pieces/Rook.java
@@ -18,59 +18,89 @@ public class Rook extends AbstractPiece {
 
         List<Move> allowedMoves = new ArrayList<>();
 
-        if (colour.equals(PlayerColour.WHITE)) {
-
-            for (int i = 1; i < 8; i++) {
-                if (moveIsAllowed(from.plus(i, 0), board)) {
+        for (int i = 1; i < 8; i++) {
+            if (moveIsInBounds(from.plus(i, 0), board)) {
+                if (spaceIsEmpty(from.plus(i, 0), board)) {
                     allowedMoves.add(new Move(from, from.plus(i, 0)));
+                } else if (spaceContainsEnemyThatCanBeTaken(from.plus(i, 0), board)) {
+                    allowedMoves.add(new Move(from, from.plus(i, 0)));
+                    break;
+                } else if (spaceContainsFriendly(from.plus(i, 0), board)) {
+                    break;
                 }
-                if (moveIsAllowed(from.plus(-i, 0), board)) {
-                    allowedMoves.add(new Move(from, from.plus(-i, 0)));
-                }
-                if (moveIsAllowed(from.plus(0, i), board)){
-                    allowedMoves.add(new Move(from, from.plus(0, i)));
-                }
-                if (moveIsAllowed(from.plus(0, -i), board)) {
-                    allowedMoves.add(new Move(from, from.plus(0, -i)));
-                }
-
+            } else {
+                break;
             }
-
+        }
+        
+        for (int i = 1; i < 8; i++) {
+            if (moveIsInBounds(from.plus(-i, 0), board)) {
+                if (spaceIsEmpty(from.plus(-i, 0), board)) {
+                    allowedMoves.add(new Move(from, from.plus(-i, 0)));
+                } else if (spaceContainsEnemyThatCanBeTaken(from.plus(-i, 0), board)) {
+                    allowedMoves.add(new Move(from, from.plus(-i, 0)));
+                    break;
+                } else if (spaceContainsFriendly(from.plus(-i, 0), board)) {
+                    break;
+                }
+            } else {
+                break;
+            }
         }
 
-
-        if (colour.equals(PlayerColour.BLACK)) {
-
-            for (int i = 1; i < 8; i++) {
-                if (moveIsAllowed(from.plus(i, 0), board)) {
-                    allowedMoves.add(new Move(from, from.plus(i, 0)));
-                }
-                if (moveIsAllowed(from.plus(-i, 0), board)) {
-                    allowedMoves.add(new Move(from, from.plus(-i, 0)));
-                }
-                if (moveIsAllowed(from.plus(0, i), board)) {
+        for (int i = 1; i < 8; i++) {
+            if (moveIsInBounds(from.plus(0, i), board)) {
+                if (spaceIsEmpty(from.plus(0, i), board)) {
                     allowedMoves.add(new Move(from, from.plus(0, i)));
+                } else if (spaceContainsEnemyThatCanBeTaken(from.plus(0, i), board)) {
+                    allowedMoves.add(new Move(from, from.plus(0, i)));
+                    break;
+                } else if (spaceContainsFriendly(from.plus(0, i), board)) {
+                    break;
                 }
-                if (moveIsAllowed(from.plus(0, -i), board)) {
-                    allowedMoves.add(new Move(from, from.plus(0, -i)));
-                }
-
+            } else {
+                break;
             }
+        }
 
+        for (int i = 1; i < 8; i++) {
+
+            if (moveIsInBounds(from.plus(0, -i), board)) {
+                if (spaceIsEmpty(from.plus(0, -i), board)) {
+                    allowedMoves.add(new Move(from, from.plus(0, -i)));
+                } else if (spaceContainsEnemyThatCanBeTaken(from.plus(0, -i), board)) {
+                    allowedMoves.add(new Move(from, from.plus(0, -i)));
+                    break;
+                } else if (spaceContainsFriendly(from.plus(0, -i), board)) {
+                    break;
+                }
+            } else {
+                break;
+            }
         }
 
         return allowedMoves;
     }
 
 
-    public boolean moveIsAllowed(Coordinates to, Board board) {
+    public boolean moveIsInBounds(Coordinates to, Board board) {
+        return (to.getCol() >= 0 && to.getCol() <= 7 && to.getRow() >= 0 && to.getRow() <= 7);
+    }
 
+    public boolean spaceIsEmpty(Coordinates to, Board board) {
 
-        if (!(to.getCol() >= 0 && to.getCol() <= 7 && to.getRow() >= 0 && to.getRow() <= 7)) {
-            return false;
-        }
+        return board.get(to) == null;
+    }
 
-        return board.get(to) == null || !board.get(to).getColour().equals(colour);
+    public boolean spaceContainsFriendly(Coordinates to, Board board) {
+
+        return board.get(to).getColour().equals(colour);
+
+    }
+
+    public boolean spaceContainsEnemyThatCanBeTaken(Coordinates to, Board board) {
+
+        return !board.get(to).getColour().equals(colour);
 
     }
 
